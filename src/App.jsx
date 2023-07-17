0da3eb036d8ce60ee50e0e00155ce12db73ff0cf
BrayanCorreaIsmael <briancorrea1@yahoo.com.ar>
2023-07-17 20:02:34 -0300

    deploay rage

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,7 @@
 import { useState } from 'react';
 import { Header } from './Components/Header/Header';
 import { ProductList } from './Components/Main/ProductList';
-import Footer from './Components/Footer/Footer'
-import Form from './Components/Form/Form';
+
 
 
 function App() {
